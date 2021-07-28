1025e89bd57be6e1bdc2b0c5453133a2baf60a2b
vaagnmazmanyan <vmazmanyanvaagn@gmail.com>
2021-07-28 16:19:38 +0400

    Update dist

---
+++ dist/page-nft-market/src/components/arrowUp.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+declare const _default: React.MemoExoticComponent<({ active }: {
+    active?: boolean | undefined;
+}) => React.ReactElement<any, string | React.JSXElementConstructor<any>>>;
+export default _default;
