9ed85fa1fa277b66047f8465abf03b18dd495d59
vaagnmazmanyan <vmazmanyanvaagn@gmail.com>
2021-07-30 17:02:40 +0400

    rollup after exporting types in index.ts

--- dist/react-hooks/src/useBalances.d.ts
+++ dist/react-hooks/src/useBalances.d.ts
@@ -1,9 +1,7 @@
-import type { DeriveBalancesAll } from '@polkadot/api-derive/types';
 import BN from 'bn.js';
 interface UseBalancesInterface {
-    balancesAll: DeriveBalancesAll | undefined;
-    deposited: BN | undefined;
-    kusamaBalancesAll: DeriveBalancesAll | undefined;
+    freeBalance: BN | undefined;
+    freeKusamaBalance: BN | undefined;
 }
-export declare const useBalances: (account: string | undefined, deposited: BN | undefined, getUserDeposit: () => Promise<BN | null>) => UseBalancesInterface;
+export declare const useBalances: (account: string | undefined, getUserDeposit: () => Promise<BN | null>) => UseBalancesInterface;
 export {};
--- packages/types/index.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2017-2021 @polkadot/react-components authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export * from '../react-components/src/types';
